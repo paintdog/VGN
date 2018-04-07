c7b73b51b105061a73421fbe5d362551a259464a
paintdog <paintdog@users.noreply.github.com>
2018-04-08 00:49:17 +0200

    Add files via upload

---
+++ vgn.py
@@ -0,0 +1,23 @@
+import datetime
+
+year = datetime.date.today().year
+
+# generating intro
+print("'''<u>V</u>or<u>g</u>angs<u>n</u>ummer''' für das Jahr {}.".format(year))
+
+# getting VGNs
+print("== Vorgangsnummern ==")
+print("{| class='wikitable'\n|-\n! style='width: 20%;' | Shortcut\n! style='width: 80%;' | Beschreibung")
+for i in range(1, 101):
+
+    vgn = "V{}{}".format(year, i)
+    
+    print("|-")
+    print("| [[{}]]".format(vgn))
+    print("| <!-- {} -->".format(vgn))
+    
+
+print("|}\n")
+
+# Generating Category
+print("[[Kategorie:Vorgang|Shortcut/{}]]".format(year))
